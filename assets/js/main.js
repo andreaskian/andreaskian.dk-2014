fc782c97e709cc76e6b79c020c3de9c87119fff2
Andreas Krohn <andreas.krohn@kraftvaerk.net>
2014-02-15 08:51:44 +0100

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+var logo = Snap.select('#personal-logo').attr({ opacity: 0 });
+
+window.onload = function () {
+
+    var parts      = [
+            ['circle', 0, 20],
+            ['right', -5, 0],
+            ['left', 5, 0],
+            ['combined', 0, 0]
+        ],
+        components = [],
+        i          = 0,
+        k          = 0;
+
+    for (i = 0; i < parts.length; i++) {
+
+        var el      = parts[i],
+            elid    = el[0],
+            element = logo.select('#' + elid);
+
+        element.attr({
+            opacity: 0,
+            transform: 't' + (el[1]) + ',' + (el[2])
+        });
+
+        components.push(element);
+
+    }
+
+    function animateLogo() {
+
+        if (!components[k]) {
+            return;
+        }
+
+        components[k].animate({
+            transform: 't' + (0) + ',' + (0),
+            opacity: 1
+        }, 250, mina.easeout);
+
+        setTimeout(animateLogo, 150);
+        k++;
+
+    }
+
+    logo.attr({ opacity: 1 });
+
+    setTimeout(animateLogo, 100);
+
+};
